3c4d99bf27d8d2ac99579835cce067bdf3dbbf0c
Attadatascientist <au2587288@gmail.com>
2025-10-02 12:40:19 +0500

    additional commit for main file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys, os
+# Ensure root directory is in Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 from app.interview import VoiceInterview
 
